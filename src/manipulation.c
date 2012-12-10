df7b57192a83a42f135f73612415eef9384ce575
tim <tim@90f4acba-837a-4d5e-b247-0d8a3d337efe>
2012-12-10 16:14:11 +0000

    Moved linked list functions to manipulation.c
    git-svn-id: http://svn.mini.lan/CS237Assignment@22 90f4acba-837a-4d5e-b247-0d8a3d337efe

--- src/manipulation.c
+++ src/manipulation.c
@@ -14,6 +14,27 @@ void checkin(Event *event, int node_ident, Entrant *entrant, int h, int m){
 	entrant->time = time;
 	listadd(at, &entrant->visited);
 }
+void listadd(void *data, LinkedList *list){
+	if(list->length==0){
+		list->head = malloc(sizeof(ListNode));
+		list->tail = list->head;
+		list->head->previous = NULL;
+	}else{
+		list->tail->next = malloc(sizeof(ListNode));
+		list->tail->next->previous = list->tail;
+		list->tail = list->tail->next;
+	}
+	list->tail->data = data;
+	list->length = list->length + 1;
+}
+void listremove(LinkedList *list, ListNode *node){
+	ListNode *previous = node->previous;
+	ListNode *next = node->next;
+	previous->next = next;
+	next->previous = previous;
+	free(node);
+	list->length = list->length - 1;
+}
 
 
 
